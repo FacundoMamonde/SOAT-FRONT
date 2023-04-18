d610e268ab0d5905cb42f3b2f81994b96c46ab3d
Carla Capelli <carlacapelli84@gmail.com>
2023-04-18 20:01:47 -0300

    logIn

--- src/components/LogInComponent.vue
+++ src/components/LogInComponent.vue
@@ -1,37 +1,40 @@
 <template>
-  <div class="container d-flex ancho">
-    <div class="soat col-sm-12 col-md-6 d-flex">
-      <div class="m-auto">
-        <h1 class="mb-0 titulo">SOAT</h1>
-        <p class="mt-0 text-center">3000</p>
+  <div class="container-fluid d-flex  p-0 min-vh-100 ancho">
+    <div class="row flex-grow-1">
+      <div class="col-md-6 d-none d-md-flex bg-light text-white justify-content-center align-items-center">
+        <div class="text-center text-dark w-100">
+          <h1 class="display-1">SOAT</h1>
+         
+        </div>
       </div>
-    </div>
-    <div class="col-sm-12 col-md-6 logIn d-flex">
-      <div class="m-auto text-center text-light">
-        <h1 class="mb-3">Bienvenido</h1>
-        <p class="my-3">Por favor ingrese sus datos para iniciar sesion</p>
-        <b-form @submit.prevent="onSubmit">
-          <b-form-input
-            class="my-3 rounded-pill text-center"
-            id="username"
-            v-model="username"
-            placeholder="Usuario/correo"
-            required
-          ></b-form-input>
-          <b-form-input
-            class="my-3 rounded-pill text-center"
-            id="password"
-            v-model="password"
-            placeholder="Contraseña"
-            type="password"
-            required
-          ></b-form-input>
-          <router-link
-            to="/pendientes"
-            class="routerLink btnSubmit rounded-pill p-2"
-            >Iniciar sesion</router-link
-          >
-        </b-form>
+  
+      <div class="col-sm-12 col-md-6 bg-dark text-light d-flex justify-content-center align-items-center">
+        <div class="text-center w-100 p-4">
+          <h1 class="mb-3">Bienvenido</h1>
+          <p class="my-3">Por favor ingrese sus datos para iniciar sesión</p>
+          <b-form @submit.prevent="onSubmit" class=" w-50 m-auto  ">
+            <b-form-input
+              class="my-3 rounded-pill text-center    "
+              id="username"
+              v-model="username"
+              placeholder="Usuario/correo"
+              required
+            ></b-form-input>
+            <b-form-input
+              class="my-3 rounded-pill text-center"
+              id="password"
+              v-model="password"
+              placeholder="Contraseña"
+              type="password"
+              required
+            ></b-form-input>
+            <router-link
+              to="/pendientes"
+              class="routerLink btnSubmit rounded-pill p-2"
+              >Iniciar sesión</router-link
+            >
+          </b-form>
+        </div>
       </div>
     </div>
   </div>
@@ -49,23 +52,15 @@ export default {
   },
   methods: {
     onSubmit() {
-      // Add your login logic here
+     
     },
   },
 };
 </script>
 
 <style scoped>
-.soat {
-  color: #233753 !important;
-  border: 1px solid #233753;
-  height: 100vh;
-}
-.logIn {
-  background-color: #233753;
-}
-.titulo {
-  font-size: 7rem;
+.min-vh-100 {
+  min-height: 100vh;
 }
 .btnSubmit {
   background-color: #4f5f75;
@@ -80,7 +75,6 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-
   z-index: 100;
   width: 100vw;
 }
