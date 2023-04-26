404d1867647f05b544e46230390fdb11f6412476
Facundo Mamonde <facundo.mamonde.96@gmail.com>
2023-04-26 20:58:55 -0300

    Se agrego API

--- src/components/OrderTableComponent.vue
+++ src/components/OrderTableComponent.vue
@@ -1,51 +1,59 @@
 <template>
   <div id="divOrderTable">
-    <b-table striped hover :items="items"></b-table>
+    <b-table responsive :items="items" :striped="striped" :fields="fields" bordered scrollX >
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+    </b-table>
+
+    <!-- <b-table striped hover :items="items"></b-table> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "OrderTableComponent",
+
+  methods: {
+    async loadAPI() {
+      this.toggleBusy()
+      const response = await fetch("https://635088ad3e9fa1244e483b3c.mockapi.io/OrderList");
+      const jsonData = await response.json();
+      this.items = (jsonData);
+      this.toggleBusy()
+      console.log(jsonData)
+    },
+    toggleBusy() {
+        this.isBusy = !this.isBusy
+      }
+  },
+  beforeMount() {
+    this.loadAPI()
+  },
   data() {
     return {
-      items: [
-        {
-          ID: 1,
-          Cliente: "Dickerson",
-          Marca: "Phillips",
-          Modelo: "asd213",
-          Falla: "no prende",
-        },
-        {
-          ID: 2,
-          Cliente: "Larsen",
-          Marca: "LG",
-          Modelo: "ghj567",
-          Falla: "instalar photoshop",
-        },
-        {
-          ID: 3,
-          Cliente: "Geneva",
-          Marca: "Samsung",
-          Modelo: "qwe123",
-          Falla: "no imprime",
-        },
-        {
-          ID: 4,
-          Cliente: "Jami",
-          Marca: "Apple",
-          Modelo: "poi324",
-          Falla: "no tiene espacio",
-        },
-      ],
+      isBusy: false,
+      fields: ['id','clienteNombre', 'equipoMarca', 'equipoModelo','falla'],
+      items: [],
     };
   },
 };
+
+
+
 </script>
 
 <style scoped>
 #divOrderTable {
   min-width: 360px;
+}
+
+.tabla {
+  max-height:300px;
+  overflow-x: scroll !important;
+  overflow-y: scroll !important;
 }
 </style>
