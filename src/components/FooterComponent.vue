1f65dcdddbc7de6c3cc8d0077b7fa58e7861e634
Facundo Mamonde <facundo.mamonde.96@gmail.com>
2023-12-01 01:50:33 -0300

    Add Linkedin

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,8 @@
 <template>
-  <p>Curso Desarrollador Full-Stack, Madariaga, 2023</p>
+  <p>Curso Desarrollador Full-Stack, Madariaga, 2023 | Desarrolladores:
+    <a href="https://www.linkedin.com/in/facundo-mamonde/" target="_blank"><i class="bi bi-linkedin"> Facundo Mamonde </i> </a> &
+    <a href="https://www.linkedin.com/in/carla-capelli-618a56214/" target="_blank"><i class="bi bi-linkedin"> Carla Capelli</i> </a>
+  </p>
 </template>
 <script>
 export default {
@@ -12,5 +15,15 @@ p {
   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
     "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
   margin: 10px;
+  text-align: center;
+}
+
+a{
+  text-decoration: none;
+  color: inherit
+}
+
+a:hover{
+  color: rgb(68, 92, 197);
 }
 </style>
